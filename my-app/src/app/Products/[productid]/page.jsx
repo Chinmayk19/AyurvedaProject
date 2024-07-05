90644ed7dcad5f7f7e239b41b9f0ac89e4402ed7
ChinmayK19 <ckulkarni198@gmail.com>
2024-07-05 23:08:53 +0530

    second commit

--- my-app/src/app/Products/[productid]/page.jsx
+++ my-app/src/app/Products/[productid]/page.jsx
@@ -57,41 +57,26 @@ const Page = () => {
     <>
       <Navbar />
       <div className="p-4">
-        {error && <div className="text-red-500">Product Not found.</div>}
-        {!data && <div>Loading...</div>}
-        {data &&
-          data
-            .filter((product) => path.includes(product._id))
-            .map((product) => (
-              <div
-                key={product._id}
-                className="flex flex-col md:flex-row justify-center border border-gray-300 p-4 rounded-lg mb-4 max-w-full md:max-w-3xl m-auto"
-              >
-                <img
-                  src={product.productImage}
-                  alt={product.productName}
-                  className="w-full md:w-1/3 h-80 object-cover rounded-md"
-                />
-                <div className="md:ml-4 mt-4 md:mt-0 w-full md:w-2/3">
-                  <h1 className="text-xl md:text-2xl font-bold">
-                    {product.productName}
-                  </h1>
-                  <p className="mt-2 text-sm md:text-base">
-                    {product.productInfo}
-                  </p>
-                  <p className="mt-2 text-lg font-semibold">
-                    {product.productPrice}
-                  </p>
-                  <button
-                    className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 w-full md:w-auto"
-                    onClick={() => handleAddToCart(product._id)}
-                  >
-                    Add to cart
-                  </button>
-                </div>
-              </div>
-            ))}
+  {error && <div className="text-red-500">Product Not found.</div>}
+  {!data && <div>Loading...</div>}
+  {data && data.filter(product => path.includes(product._id)).map(product => (
+    <div key={product._id} className="flex flex-col md:flex-row justify-center border border-gray-300 p-4 rounded-lg mb-4 max-w-full md:max-w-3xl m-auto">
+      <img src={product.productImage} alt={product.productName} className="w-full md:w-1/3 h-80 object-cover rounded-md" />
+      <div className="md:ml-4 mt-4 md:mt-0 w-full md:w-2/3">
+        <h1 className="text-xl md:text-2xl font-bold">{product.productName}</h1>
+        <p className="mt-2 text-sm md:text-base">{product.productInfo}</p>
+        <p className="mt-2 text-lg font-semibold">{product.productPrice}</p>
+        <button
+          className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 w-full md:w-auto"
+          onClick={() => handleAddToCart(product._id)}
+        >
+          Add to cart
+        </button>
       </div>
+    </div>
+  ))}
+</div>
+
     </>
   );
 };
