8975acfaa8d98ad436f16a92c0037d9b7cfcd230
ChinmayK19 <ckulkarni198@gmail.com>
2024-07-07 19:43:09 +0530

    third commit

--- my-app/src/app/Components/ProductCard/index.jsx
+++ my-app/src/app/Components/ProductCard/index.jsx
@@ -1,9 +1,11 @@
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 
 const Index = ({ product }) => {
-  const router=useRouter();
+
   return (
-    <div className="product-card max-w-xs w-full h-96 md:max-w-md mx-auto mb-8 p-4 border rounded-lg shadow-md bg-white cursor-pointer" onClick={()=>{router.push(`/Products/${product._id}`)}}>
+  <Link href={`/Products/${product._id}`}>
+    <div className="product-card max-w-xs w-full h-96 md:max-w-md mx-auto mb-8 p-4 border rounded-lg shadow-md bg-white cursor-pointer">
       <h2 className="text-xl font-bold mb-2">{product.productName}</h2>
       <img src={product.productImage} alt={product.productName} className="w-full h-48 object-cover mb-4 rounded-md" />
       <p className="text-lg text-gray-700 mb-2">Price: ${product.productPrice}</p>
@@ -13,6 +15,7 @@ const Index = ({ product }) => {
         <p className="text-red-600">Not Available</p>
       )}
     </div>
+  </Link>
   );
 };
 
